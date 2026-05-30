5d6d5bb276dcfa42df877bb3c694fd7bca838449
Decibel <takabee310634@gmail.com>
2026-05-30 15:08:35 +0900

    計算部分を担うcalculations.pyを作成

---
+++ calculations.py
@@ -0,0 +1,59 @@
+"""電圧降下計算に関連する定数と計算ロジックを管理するモジュール。
+
+電線の規格(断面積)や抵抗値のデータを提供し、
+それらに基づいた電圧降下および電圧降下率の計算を行います。
+"""
+
+# 電線抵抗(Ω/km)のデータ
+SQR = [1.5, 2.5, 4, 6, 10, 16, 25, 35, 50, 70, 95, 120, 150, 185]
+R20km = [
+    12.2,
+    7.56,
+    4.7,
+    3.11,
+    1.84,
+    1.16,
+    0.734,
+    0.529,
+    0.391,
+    0.27,
+    0.195,
+    0.154,
+    0.126,
+    0.1,
+]
+R20m = dict(zip(SQR, R20km, strict=True))
+
+
+class VoltageDropCalculator:
+    """電圧降下の計算を行うためのクラス。
+
+    三相交流や直流・単相交流の計算ロジックを提供し、
+    電線の抵抗値データに基づいて電圧降下を算出します。
+    """
+
+    def __init__(self) -> None:
+        """VoltageDropCalculator クラスを初期化します。"""
+
+    def v_drop_3p(self, current: float, sqr: float, length: float, num: int) -> float:
+        """三相交流の電圧降下を計算する関数"""
+        vd = R20m[sqr] * 1.3 * 2 * length * (current / num) * (1.732 / 2) / 1000
+        return vd
+
+    def v_drop_dc(self, current: float, sqr: float, length: float, num: int) -> float:
+        """単相・直流の電圧降下を計算する関数"""
+        vd = R20m[sqr] * 1.3 * 2 * length * (current / num) / 1000
+        return vd
+
+    def v_drop(
+        self,
+        r: int,
+        current: float,
+        sqr: float,
+        length: float,
+        num: int,
+    ) -> float:
+        """三相交流か単相・直流かを判断して、適切な電圧降下を計算する関数"""
+        if r == 0:
+            return self.v_drop_3p(current, sqr, length, num)
+        return self.v_drop_dc(current, sqr, length, num)
